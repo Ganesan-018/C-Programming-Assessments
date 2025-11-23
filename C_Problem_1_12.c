812d4dcbd150a3dcb4801176c818b4b20e6b02f2
Ganesan-018 <ganesan.mit.ec@gmail.com>
2025-11-23 23:40:46 +0530

    Create C_Problem_1_12.c

---
+++ C_Problem_1_12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int num,digit,sum=0;
+    printf("Enter a three digit number : ");
+    scanf("%d",&num);
+    while(num>0)
+    {
+        digit=num%10;
+        sum=sum+digit;
+        num=num/10;
+    }
+    printf("Sum of the digits : %d",sum);
+    return 0;
+}
