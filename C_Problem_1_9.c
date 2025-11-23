3a0878bca59a4a958bdb957b0451d2e36bc499ef
Ganesan-018 <ganesan.mit.ec@gmail.com>
2025-11-23 23:17:37 +0530

    Create C_Problem_1_9.c

---
+++ C_Problem_1_9.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    int num;
+    printf("Enter a three digit number : ");
+    scanf("%d",&num);
+    printf("Hundred's digit of a number : %d", num/100);
+    return 0;
+}
