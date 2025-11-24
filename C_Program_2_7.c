7bc3b6b810398b886652f9cc6ebd4b19125fc2ac
Ganesan-018 <ganesan.mit.ec@gmail.com>
2025-11-24 23:37:35 +0530

    Create C_Program_2_7.c

---
+++ C_Program_2_7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a two digit number : ");
+    scanf("%d",&num);
+    num = (num/10)==(num%10) ? 1 : 0;
+    printf("Output : %d",num);
+    return 0;
+}
