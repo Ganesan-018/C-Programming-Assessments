2c55dc9d8e3513defe0ffb2488a8b79735f532c2
Ganesan-018 <ganesan.mit.ec@gmail.com>
2025-11-25 00:14:30 +0530

    Create C_Program_3_8.c

---
+++ C_Program_3_8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a two digit number : ");
+    scanf("%d",&num);
+    if(num/10 != num%10)
+        printf("Success");
+    else
+        printf("Failure");
+}
