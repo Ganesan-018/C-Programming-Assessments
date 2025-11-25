8199f1dda1a8ea6b6049b0d9a736f8d19b5638a6
Ganesan-018 <ganesan.mit.ec@gmail.com>
2025-11-25 23:22:38 +0530

    Create C_Problem_5_9.c

---
+++ C_Problem_5_9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int x,sum=0;
+    for(x=10;x<100;x++)
+    {
+        if(x%10 == 5)
+            sum+=x;
+    }
+    printf("Sum : %d",sum);
+    return 0;
+}
