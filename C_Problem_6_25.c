49d1ec19655e9a28b5ca1a1d892fc7605b06260d
Ganesan-018 <ganesan.mit.ec@gmail.com>
2025-11-26 21:13:45 +0530

    Create C_Problem_6_25.c

---
+++ C_Problem_6_25.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int x,count=0,digit,count_prime=0;
+    printf("Enter a number : ");
+    scanf("%d",&x);
+    while(x!=0)
+    {
+        count=0;
+        digit=x%10;
+        for(int i=1;i<=digit;i++)
+            if(x%i==0)
+                count++;
+        x=x/10;
+        if(count==2)
+            count_prime++;
+    }
+    printf("Output : %d",count_prime);
+    return 0;
+}
