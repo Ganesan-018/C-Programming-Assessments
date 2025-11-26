e68acdef1a8c7e22e2b8c4a4522d1bd5b3c2a583
Ganesan-018 <ganesan.mit.ec@gmail.com>
2025-11-26 12:16:03 +0530

    Create C_Problem_6_7.c

---
+++ C_Problem_6_7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int x=11,digit,sum,temp;
+    while(x<100)
+    {
+        if(x%2!=0)
+        {
+            sum=0;
+            temp=x;
+            while(temp!=0)
+            {
+                digit=temp%10;
+                sum+=digit;
+                temp=temp/10;
+            }
+            if(sum==7)
+                printf("%d\n",x);
+        }
+        x++;
+    }
+    return 0;
+}
