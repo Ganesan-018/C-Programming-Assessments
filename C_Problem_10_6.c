9c15350f0292e9ba5b0311d1695dac67bf5386ec
Ganesan-018 <ganesan.mit.ec@gmail.com>
2025-11-27 16:13:26 +0530

    Create C_Problem_10_6.c

---
+++ C_Problem_10_6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int num,temp,digit,i=0;
+    char str[50],ch;
+    printf("Enter a integer : ");
+    scanf("%d",&num);
+    temp=num;
+    if(num==0)
+        str[i]='0';
+    while(temp!=0)
+    {
+        digit=temp%10;
+        str[i]=digit+'0';
+        temp=temp/10;
+        i++;
+    }
+    str[i]='\0';
+    for(int j=0,k=i-1;j<k;j++,k--)
+    {
+        ch=str[j];
+        str[j]=str[k];
+        str[k]=ch;
+    }
+    for(int i=0;str[i]!='\0';i++)
+        printf("%c\n",str[i]);
+    return 0;
+}
